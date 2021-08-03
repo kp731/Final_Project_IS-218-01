dd402ffc2e3e922d45cbbb534f3e76e9475445a7
kp731 <78096329+kp731@users.noreply.github.com>
2021-08-02 22:40:32 -0400

    Step - 2 Rendering Pages in Flask using Jinja

--- Readme.md
+++ Readme.md
@@ -1,1 +1,5 @@
-![step1](Screenshots/step1.PNG)
+#Step 2 Part - 1 
+![step2](Screenshots/Step-2-Part-1.PNG)
+
+# Step 2 Part - 2
+![step2](Screenshots/Step-2-Part-2.PNG)
---
+++ app/routes.py
@@ -0,0 +1,126 @@
+from typing import List, Dict
+import simplejson as json
+from flask import Flask, request, Response, redirect
+from flask import render_template
+from flask import current_app as app
+from pymysql.cursors import DictCursor
+from flaskext.mysql import MySQL
+from app.app import mysql
+
+
+@app.route('/', methods=['GET'])
+def index():
+    user = {'username': "Kartavya's Project"}
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM airtravel')
+    result = cursor.fetchall()
+    return render_template('index.html', title='Home', user=user, travels=result)
+
+
+@app.route('/view/<int:travel_id>', methods=['GET'])
+def record_view(travel_id):
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM airtravel WHERE id=%s', travel_id)
+    result = cursor.fetchall()
+    return render_template('view.html', title='View Form', travel=result[0])
+
+
+@app.route('/edit/<int:travel_id>', methods=['GET'])
+def form_edit_get(travel_id):
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM airtravel WHERE id=%s', travel_id)
+    result = cursor.fetchall()
+    return render_template('edit.html', title='Edit Form', travel=result[0])
+
+
+@app.route('/edit/<int:travel_id>', methods=['POST'])
+def form_update_post(travel_id):
+    cursor = mysql.get_db().cursor()
+    inputData = (request.form.get('Month'), request.form.get('Column_1958'), request.form.get('Column_1959'),
+                 request.form.get('Column_1960'), travel_id)
+    sql_update_query = """UPDATE airtravel t SET t.Month = %s, t.Column_1958 = %s, t.Column_1959 = %s, t.Column_1960 = 
+    %s  WHERE t.id = %s """
+    cursor.execute(sql_update_query, inputData)
+    mysql.get_db().commit()
+    return redirect("/", code=302)
+
+@app.route('/travel/new', methods=['GET'])
+def form_insert_get():
+    return render_template('new.html', title='New Person Form')
+
+
+@app.route('/travel/new', methods=['POST'])
+def form_insert_post():
+    cursor = mysql.get_db().cursor()
+    inputData = (request.form.get('Month'), request.form.get('Column_1958'), request.form.get('Column_1959'),
+                 request.form.get('Column_1960'))
+    sql_insert_query = """INSERT INTO airtravel (Month,Column_1958,Column_1959,Column_1960) VALUES (%s, %s,%s, %s) """
+    cursor.execute(sql_insert_query, inputData)
+    mysql.get_db().commit()
+    return redirect("/", code=302)
+
+@app.route('/delete/<int:airtravel_id>', methods=['POST'])
+def form_delete_post(airtravel_id):
+    cursor = mysql.get_db().cursor()
+    sql_delete_query = """DELETE FROM airtravel WHERE id = %s """
+    cursor.execute(sql_delete_query, airtravel_id)
+    mysql.get_db().commit()
+    return redirect("/", code=302)
+
+
+@app.route('/api/v1/travel', methods=['GET'])
+def api_browse() -> str:
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM airtravel')
+    result = cursor.fetchall()
+    json_result = json.dumps(result);
+    resp = Response(json_result, status=200, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/v1/travel/<int:travel_id>', methods=['GET'])
+def api_retrieve(travel_id) -> str:
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM airtravel WHERE id=%s', travel_id)
+    result = cursor.fetchall()
+    json_result = json.dumps(result);
+    resp = Response(json_result, status=200, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/v1/travel/', methods=['POST'])
+def api_add() -> str:
+    content = request.json
+    cursor = mysql.get_db().cursor()
+    inputData = (content['Month'], content['Column_1958'], content['Column_1959'], content['Column_1960'])
+    sql_insert_query = """INSERT INTO airtravel(Month,Column_1958,Column_1959,Column_1960) VALUES (%s, %s,%s, %s)"""
+    cursor.execute(sql_insert_query, inputData)
+    mysql.get_db().commit()
+    resp = Response(status=201, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/v1/travel/<int:travel_id>', methods=['PUT'])
+def api_edit(travel_id) -> str:
+    cursor = mysql.get_db().cursor()
+    content = request.json
+    inputData = (content['Month'], content['Column_1958'], content['Column_1959'], content['Column_1960'], travel_id)
+    sql_update_query = """UPDATE airtravel t SET t.Month=%s, t.Column_1958=%s, t.Column_1959=%s, t.Column_1960=%s WHERE t.id = %s"""
+    cursor.execute(sql_update_query, inputData)
+    mysql.get_db().commit()
+    resp = Response(status=200, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/travel/<int:travel_id>', methods=['DELETE'])
+def api_delete(travel_id) -> str:
+    cursor = mysql.get_db().cursor()
+    sql_delete_query = """DELETE FROM airtravel WHERE id=%s"""
+    cursor.execute(sql_delete_query, travel_id)
+    mysql.get_db().commit()
+    resp = Response(status=200, mimetype='application/json')
+    return resp
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
