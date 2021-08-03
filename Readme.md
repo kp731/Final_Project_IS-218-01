b7d9c58cfa5484b9ca0e8f71d852f3989045d708
kp731 <78096329+kp731@users.noreply.github.com>
2021-08-02 23:45:07 -0400

    Step - 6 Demystifying Flask's Application Factory

--- Readme.md
+++ Readme.md
@@ -1,5 +1,8 @@
-#Step 5 Part - 1
-![step3](Screenshots/Step-5-Part-1.PNG) 
+#Step 6 Part - 1
+![step3](Screenshots/Step-6-Part-1.PNG) 
 
-#Step 5 Part - 2
-![step3](Screenshots/Step-5-Part-2.PNG) 
+#Step 6 Part - 2
+![step3](Screenshots/Step-6-Part-2.PNG) 
+
+#Step 6 Part - 3
+![step3](Screenshots/Step-6-Part-3-01.PNG) 
--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,20 @@
-
 """Initialize Flask Application."""
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_redis import FlaskRedis
 
+db = SQLAlchemy()
+r = FlaskRedis()
 
-def create_app():
+def init_app():
     """Construct the core application."""
-    app = Flask(__name__, template_folder="templates")
+    app = Flask(__name__, instance_relative_config=False)
+    app.config.from_object('config.Config')
 
+    db.init_app(app)
+    r.init_app(app)
     with app.app_context():
         from . import routes
-
+        app.register_blueprint(auth.auth_bp)
+        app.register_blueprint(admin.admin_bp)
         return app
